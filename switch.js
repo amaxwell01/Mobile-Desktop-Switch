ae3f193efcc6bf3bc3c101b355c93570a96261b3
Andrew Maxwell <amaxwell01@gmail.com>
2013-12-27 21:28:27 -0800

    Cleaned up the code and brought it back to life. It still needs tests and further refinements

--- switch.js
+++ switch.js
@@ -1,7 +1,71 @@
-$(document).ready( function()
-{
-    $('#setCookie').click(function()
-    {
-        $(cookie).('viewAnyways', true);
-    });
+var mobileSwitch = {
+    checkMobileCookie: function() {
+        var desktopView = $.cookie('desktop_view'),
+            viewFullSiteButton = $('#view_full_site'),
+            myBody = $('body');
+
+        if ( !(jQuery.browser.mobile) && typeof(desktopView) === 'undefined' ||
+             desktopView === 'true' ) {
+            myBody.removeClass('mobile_friendly');
+            viewFullSiteButton.text('[View Mobile Site]');
+        } else {
+            myBody.addClass('mobile_friendly');
+            viewFullSiteButton.text('[View Full Site]');
+        }
+    },
+
+    checkDevice: function() {
+        var myBody = $('body');
+
+        if ( jQuery.browser.mobile ) {
+            myBody.addClass('mobile_friendly');
+        } else {
+            myBody.removeClass('mobile_friendly');
+        }
+    },
+
+    init: function() {
+        var viewOther = $('#view_full_site');
+
+        $('#view_full_site').on('click', function() {
+            mobileSwitch.viewFullSite();
+        });
+
+        this.checkDevice();
+        this.checkMobileCookie();
+        this.fancybox();
+        this.moveWelcomeContent();
+        this.resizeHeader();
+
+        // Check if it is mobile
+        if ( jQuery.browser.mobile ) {
+            viewOther.css({
+                display: 'block'
+            });
+        } else {
+            viewOther.css({
+                display: 'none'
+            });
+        }
+    },
+
+    viewFullSite: function() {
+        var desktopView = $.cookie('desktop_view');
+
+        if ( desktopView === 'true' ) {
+            desktopView = false;
+        } else {
+            desktopView = true;
+        }
+
+        $.cookie('desktop_view', desktopView, {
+            path: '/'
+        });
+
+        this.checkMobileCookie();
+    }
+};
+
+$(function() {
+    mobileSwitch.init();
 });
